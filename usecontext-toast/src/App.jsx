aaeffa1aacd41152aa5403c4f4c1c8a4f0f35c27
simo994q <simonkn1@hotmail.com>
2023-09-07 08:46:48 +0200

    setup start

---
+++ usecontext-toast/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState, useContext } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+import { SuccessPage } from './components/SuccessPage'
+import { InfoPage } from './components/InfoPage'
+import { WarningPage } from './components/WarningPage'
+import { ErrorPage } from './components/ErrorPage'
+
+function App() {
+  return (
+    <>
+      
+
+    </>
+  )
+}
+
+export default App
